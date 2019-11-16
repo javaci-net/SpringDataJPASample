1fa30611d508beec26852c75f0dc12cc5d03fb14
ozkan.sari <ozkan.sari@NB27AEEE.cardtek.local>
2019-11-16 23:29:17 +0300

    Entities

---
+++ src/main/java/net/javaci/dbsample/springdatajpa1/entity/Release.java
@@ -0,0 +1,11 @@
+package net.javaci.dbsample.springdatajpa1.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Release {
+
+	@Id
+    private Integer id;
+}
