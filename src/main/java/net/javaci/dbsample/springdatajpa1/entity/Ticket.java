260a8aa8b952f3818f94cc2ca0e0dcd8caf124fb
ozkansari <ozkansari@gmail.com>
2019-11-17 13:53:09 +0300

    Transient field

--- src/main/java/net/javaci/dbsample/springdatajpa1/entity/Ticket.java
+++ src/main/java/net/javaci/dbsample/springdatajpa1/entity/Ticket.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Ticket {
@@ -22,5 +23,8 @@ public class Ticket {
 	private String title;
 	private LocalDate createDate;
 	private LocalDateTime createDateTime;
+	
+	@Transient
+	private int initialLengthDescription;
 	
 }
