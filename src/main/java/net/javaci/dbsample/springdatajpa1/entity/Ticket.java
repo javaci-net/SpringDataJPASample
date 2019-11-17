fc76f1af46d0d3484e719851deaffe1eee3085fb
ozkansari <ozkansari@gmail.com>
2019-11-17 13:11:26 +0300

    Ticket Entity v1

--- src/main/java/net/javaci/dbsample/springdatajpa1/entity/Ticket.java
+++ src/main/java/net/javaci/dbsample/springdatajpa1/entity/Ticket.java
@@ -1,11 +1,19 @@
 package net.javaci.dbsample.springdatajpa1.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Ticket {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+	private Integer appId;
+	private String description;
+	private String status;
+	private String title;
+	
 }
