eb4fb0101b49e0ca81e71c378fa76ee96ac84b3c
ozkansari <ozkansari@gmail.com>
2019-11-24 15:47:57 +0300

    ticket and release spring data repo implementations

---
+++ src/main/java/net/javaci/dbsample/springdatajpa1/dao/net/javaci/dbsample/springdatajpa1/dao/springdata/ReleaseDAOCrudRepositoryImpl.java
@@ -0,0 +1,31 @@
+package net.javaci.dbsample.springdatajpa1.dao.net.javaci.dbsample.springdatajpa1.dao.springdata;
+
+import javax.transaction.Transactional;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.javaci.dbsample.springdatajpa1.dao.ReleaseDAO;
+import net.javaci.dbsample.springdatajpa1.entity.Release;
+
+@Transactional
+@Repository
+@Primary
+public interface ReleaseDAOCrudRepositoryImpl extends ReleaseDAO, JpaRepository<Release, Integer> {
+
+	/** @deprecated Use save */
+	@Deprecated
+	@Override
+	default void addRelease(Release release) {
+		save(release);
+	}
+
+	/** @deprecated Use findById */
+	@Deprecated
+	@Override
+	default Release getReleaseById(int releaseId) {
+		return findById(releaseId).get();
+	}
+
+}
